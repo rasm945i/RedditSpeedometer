7fa991e948c340e89245bd5878d16c219af73c3b
rasm945i <rasmusb8b@gmail.com>
2022-10-24 17:05:16 +0200

    Support for custom messages

--- src/main/java/dk/rasmusbendix/redditspeedometer/SpeedometerCommand.java
+++ src/main/java/dk/rasmusbendix/redditspeedometer/SpeedometerCommand.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public record SpeedometerCommand(SpeedometerUpdater updater, boolean allowToggle) implements CommandExecutor {
+public record SpeedometerCommand(RedditSpeedometer plugin, boolean allowToggle) implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
@@ -18,14 +18,33 @@ public record SpeedometerCommand(SpeedometerUpdater updater, boolean allowToggle
         }
 
         if (!allowToggle) {
-            sender.sendMessage("Toggling the speedometer is not allowed.");
+            sender.sendMessage(
+                    ChatColor.translateAlternateColorCodes('&',
+                            plugin.getConfig().getString(
+                                    "messages.toggling-not-allowed",
+                                    "&cToggling the speedometer is not allowed."
+                            )
+                    )
+            );
             return true;
         }
 
-        boolean result = updater.toggleSpeedometer(((Player) sender));
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eSpeedometer is now " +
-                (result ? "&aenabled" : "&cdisabled") + "&e!"));
+        // Default to disabled message
+        String message = plugin.getConfig().getString(
+                "messages.disabled-speedometer",
+                "&eSpeedometer is now &cdisabled&e!"
+        );
 
+        // If speedometer gets enabled, change the message accordingly :)
+        if (plugin.getSpeedometerUpdater().toggleSpeedometer(((Player) sender))) {
+            message = plugin.getConfig().getString(
+                    "messages.enabled-speedometer",
+                    "&eSpeedometer is now &aenabled&e!"
+            );
+        }
+
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
         return true;
+
     }
 }
