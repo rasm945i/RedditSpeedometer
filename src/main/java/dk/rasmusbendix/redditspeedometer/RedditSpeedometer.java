375c42ea6c424f93298996b71f6449aa00118480
rasm945i <rasmusb8b@gmail.com>
2022-10-24 16:46:11 +0200

    Register PAPI expansion

--- src/main/java/dk/rasmusbendix/redditspeedometer/RedditSpeedometer.java
+++ src/main/java/dk/rasmusbendix/redditspeedometer/RedditSpeedometer.java
@@ -1,23 +1,40 @@
 package dk.rasmusbendix.redditspeedometer;
 
+import dk.rasmusbendix.redditspeedometer.listener.DisconnectListener;
+import dk.rasmusbendix.redditspeedometer.listener.JoinListener;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RedditSpeedometer extends JavaPlugin {
 
-    @Getter private SpeedometerUpdater speedometerUpdater;
+    @Getter
+    private SpeedometerUpdater speedometerUpdater;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         saveDefaultConfig();
         speedometerUpdater = new SpeedometerUpdater(this);
-        this.getServer().getPluginCommand("speedometer").setExecutor(new SpeedometerCommand(speedometerUpdater));
+
+        // Listeners to properly keep track of joining and leaving players, so we don't store unnecessary information
+        new JoinListener(this);
         new DisconnectListener(this);
+
+        // Toggle command
+        boolean allowToggle = getConfig().getBoolean("allow-toggle", true);
+        getLogger().info("Allow toggling speedometer: " + allowToggle);
+        if (allowToggle) {
+            //noinspection ConstantConditions
+            this.getServer().getPluginCommand("speedometer").setExecutor(new SpeedometerCommand(speedometerUpdater));
+        }
+
+        // Register expansion if PAPI is present
+        if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new PapiExpansion(this).register();
+        }
+
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+    public void onDisable() {}
 }
